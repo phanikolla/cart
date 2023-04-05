5d2cb377af14acac7204c195c92806b3ba49f661
phani <phani.kollas@gmail.com>
2023-04-05 20:21:20 +0530

    Remove and Delete Functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,6 +4,12 @@ class CartItem extends React.Component {
     render() {
         console.log('this.props', this.props);
         const { price, title, qty } = this.props.product;
+        const {
+            product,
+            onIncreaseQuantity,
+            onDecreaseQuantity,
+            onDeleteProduct
+        } = this.props;
         return (
             <div className="cart-item">
                 {this.props.jsx}
@@ -20,18 +26,19 @@ class CartItem extends React.Component {
                             alt="increase"
                             className="action-icons"
                             src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+                            onClick={() => onIncreaseQuantity(product)}
                         />
                         <img
                             alt="decrease"
                             className="action-icons"
                             src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
-                            onClick={this.decreaseQuantity}
+                            onClick={() => onDecreaseQuantity(product)}
                         />
                         <img
                             alt="delete"
                             className="action-icons"
                             src="https://cdn-icons-png.flaticon.com/512/1214/1214926.png"
+                            onClick={() => onDeleteProduct(product.id)}
                         />
                     </div>
                 </div>
