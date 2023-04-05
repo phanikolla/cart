e5e48a27370ec1ab05a9f0c73f891a9d46c7cec4
phani <phani.kollas@gmail.com>
2023-04-05 20:11:26 +0530

    Increasing the quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,86 +1,52 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  
-// testing () {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve('done');
-  //     }, 5000);
-  //   })
-
-  //   promise.then(() => {
-  //     // setState acts like a synchronus call
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     console.log('state', this.state);
-  //   });
-  // }
-increaseQuantity = () => {
-    // this.state.qty += 1;
-    // console.log('this', this.state);
-    // setState form 1
-    // this.setState({
-    //   qty: this.state.qty + 1
-    // }, () => {});
-
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-        return {
-            qty: prevState.qty + 1
-        }
-    }, () => {
-        console.log('this.state', this.state);
-    });
-}
-
-    decreaseQuantity = () => {
-        const { qty } = this.state;
-
-        if (qty == 0)
-            return;
-        
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        });
-    }
     render() {
-    console.log('this.props', this.props);
-    const { price, title, qty } = this.props.product;
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img style={styles.image} />
-        </div>
-        <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-                    <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" onClick={this.increaseQuantity}/>
-                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" onClick={this.decreaseQuantity} />
-                    <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214926.png" />
-          </div>
-        </div>
-      </div>
-    );
-  }
+        console.log('this.props', this.props);
+        const { price, title, qty } = this.props.product;
+        return (
+            <div className="cart-item">
+                {this.props.jsx}
+                <div className="left-block">
+                    <img style={styles.image} />
+                </div>
+                <div className="right-block">
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price} </div>
+                    <div style={{ color: '#777' }}>Qty: {qty} </div>
+                    <div className="cart-item-actions">
+                        {/* Buttons */}
+                        <img
+                            alt="increase"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+                        />
+                        <img
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+                            onClick={this.decreaseQuantity}
+                        />
+                        <img
+                            alt="delete"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214926.png"
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 const styles = {
-  image: {
-    height: 110,
-    width: 110,
-    borderRadius: 4,
-    background: '#ccc'
-  }
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background: '#ccc'
+    }
 }
 
 export default CartItem;
